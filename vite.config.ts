bf6262e7728c389b135637df4062640209f853f2
Emre Kentli <emrekentlii@gmail.com>
2026-08-24 02:34:38 +0300

    Enable inline dynamic imports in Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'pages' ? '/moradon-fortune/' : '/',
+  build: {
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
   server: {
     watch: {
       ignored: ['**/.chrome-preview-profile*/**', '**/*runtime-check*.png', '**/preview*.png', '**/references/**'],
